0fd19c1902858edb78d256f1213662b0090351f4
RamanYakshevich <s227736@sggw.edu.pl>
2026-08-10 11:39:10 +0200

    add event schema

---
+++ src/schemas/event.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class EventBase(BaseModel):
+    title: str = Field(
+        ...,
+        min_length=3,
+        max_length=255,
+        description="Event name",
+        examples=["Python Meetup Warsaw"]
+    )
+    description: str = Field(
+        ...,
+        min_length=10,
+        description="Event description",
+        examples=["Async Python and FastAPI course"]
+    )
+    location: str = Field(
+        ...,
+        description="Venue of the event"
+    )
+    event_date: int = Field(
+        ...,
+        description="Date and time of the event"
+    )
+    price: int = Field(
+        ...,
+        gt=0,
+        description="Ticket price in cents",
+        examples=[1500]
+    )
+    total_seats: int = Field(
+        ...,
+        gt=0,
+        description="Number of seats",
+        examples=[100]
+    )
+
+
+class EventCreate(EventBase):
+    pass
+
+class EventUpdate(BaseModel):
+    title: Optional[str] = Field(None, min_length=3, max_length=255)
+    description: Optional[str] = Field(None, min_length=10)
+    location: Optional[str] = Field(None, min_length=2, max_length=255)
+    event_date: Optional[datetime] = None
+    price: Optional[int] = Field(None, ge=0)
+    total_seats: Optional[int] = Field(None, gt=0)
+
+class EventRead(EventBase):
+    id: int
+    available_seats: int
+    created_ad: datetime
+    updated_at: datetime
+
+    model_config = {"from_attributes": True}
